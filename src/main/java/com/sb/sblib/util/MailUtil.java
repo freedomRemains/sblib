1e8ee0e1ab7e1be9f9a67226c8faa3a6a7509fd4
suteado90573 <suteado90573@gmail.com>
2025-12-07 13:08:14 +0900

    #13 MailUtil及びコンポーネント全般の見直し

--- src/main/java/com/sb/sblib/util/MailUtil.java
+++ src/main/java/com/sb/sblib/util/MailUtil.java
@@ -13,37 +13,43 @@ public class MailUtil {
     /** メール送信 */
     private final JavaMailSender mailSender;
 
-    /** メール */
-    private SimpleMailMessage simpleMailMessage;
+    public class Mail {
 
-    public MailUtil createMail() {
-        simpleMailMessage = new SimpleMailMessage();
-        return this;
+        /** メール */
+        private SimpleMailMessage simpleMailMessage;
+
+        public Mail() {
+            simpleMailMessage = new SimpleMailMessage();
+        }
+
+        public Mail from(String from) {
+            simpleMailMessage.setFrom(from);
+            return this;
+        }
+
+        public Mail to(String... to) {
+            simpleMailMessage.setTo(to);
+            return this;
+        }
+
+        public Mail cc(String... cc) {
+            simpleMailMessage.setCc(cc);
+            return this;
+        }
+
+        public Mail bcc(String... bcc) {
+            simpleMailMessage.setBcc(bcc);
+            return this;
+        }
+
+        public void send(String title, String honbun) {
+            simpleMailMessage.setSubject(title);
+            simpleMailMessage.setText(honbun);
+            mailSender.send(simpleMailMessage);
+        }
     }
 
-    public MailUtil from(String from) {
-        simpleMailMessage.setFrom(from);
-        return this;
-    }
-
-    public MailUtil to(String... to) {
-        simpleMailMessage.setTo(to);
-        return this;
-    }
-
-    public MailUtil cc(String... cc) {
-        simpleMailMessage.setCc(cc);
-        return this;
-    }
-
-    public MailUtil bcc(String... bcc) {
-        simpleMailMessage.setBcc(bcc);
-        return this;
-    }
-
-    public void send(String title, String honbun) {
-        simpleMailMessage.setSubject(title);
-        simpleMailMessage.setText(honbun);
-        mailSender.send(simpleMailMessage);
+    public Mail createMail() {
+        return new Mail();
     }
 }
